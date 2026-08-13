cf4da2afa625ca1f00696bc2a0ad6be47d7db434
Harsh Sanjay Yeole <149146779+Harsh-Yeole@users.noreply.github.com>
2026-08-13 09:28:58 +0530

    Time: 331 ms (5.34%), Space: 72.1 MB (6.05%) - LeetHub

--- 3154-find-number-of-ways-to-reach-the-k-th-stair/3154-find-number-of-ways-to-reach-the-k-th-stair.cpp
+++ 3154-find-number-of-ways-to-reach-the-k-th-stair/3154-find-number-of-ways-to-reach-the-k-th-stair.cpp
@@ -1,36 +1,34 @@
 class Solution {
 public:
-    unordered_map<string, int> t;
-
-    int solve(int i, int jump, int k, bool canJump, vector<long long>& power) {
-        if (i > k + 1)
-            return 0;
-
-        string key = to_string(i) + "_" + to_string(jump) + "_" + to_string(canJump);
-
-        if (t.find(key) != t.end())
-            return t[key];
-
-        int res = 0;
-
-        if (i == k)
-            res += 1;
-
-        if (canJump) {
-            res += solve(i - 1, jump, k, 0, power);
+    int f(int i,int jump,int can,map<vector<int>,int>&m,int k,vector<int>&pow){
+        if(i>k+1)
+        return 0;
+        if(m.count({i,jump,can})!=0){
+            return m[{i,jump,can}];
         }
-
-        res += solve(i + power[jump], jump + 1, k, 1, power);
-
-        return t[key] = res;
+        int total=0;
+        if(i==k)
+        total++;
+        if(can){
+            total+=f(i-1,jump,0,m,k,pow);
+            int cal=pow[jump];
+            total+=f(i+cal,jump+1,1,m,k,pow);
+        }
+        else{
+            int cal=pow[jump];
+            total+=f(i+cal,jump+1,1,m,k,pow);
+        }
+        return m[{i,jump,can}]=total;
     }
-
     int waysToReachStair(int k) {
-        vector<long long> power(32);
-        for (int i = 0; i <= 31; i++) {
-            power[i] = pow(2, i);
+       map<vector<int>,int>m; 
+        vector<int>pow;
+        int p=1;
+        pow.push_back(p);
+        for(int i=1;i<=30;i++){
+            p=p*2;
+            pow.push_back(p);
         }
-
-        return solve(1, 0, k, 1, power);
+        return f(1,0,1,m,k,pow);
     }
 };

1d4a0ed3ee4cfc1b890d8d4e6b8a86bff0dd6966
Harsh Sanjay Yeole <149146779+Harsh-Yeole@users.noreply.github.com>
2026-08-21 14:13:49 +0530

    Time: 0 ms (100%), Space: 50.3 MB (17.08%) - LeetHub

--- 1901-find-a-peak-element-ii/1901-find-a-peak-element-ii.cpp
+++ 1901-find-a-peak-element-ii/1901-find-a-peak-element-ii.cpp
@@ -1,57 +1,25 @@
 class Solution {
 public:
-    pair<int,int> peak(int m,int n,int r,int c,vector<vector<int>>&mat){
-        int mx=mat[r][c];
-        int i=r,j=c;
-        if(r<m-1){
-            if(mat[r+1][c]>mx){
-                mx=mat[r+1][c];
-                i=r+1;
-                j=c;
-            }
-        }
-        if(c<n-1){
-            if(mat[r][c+1]>mx){
-                mx=mat[r][c+1];
-                i=r;
-                j=c+1;
-            }
-        }
-        if(r>0){
-            if(mat[r-1][c]>mx){
-                mx=mat[r-1][c];
-                i=r-1;
-                j=c;
-            }
-        }
-        if(c>0){
-            if(mat[r][c-1]>mx){
-                mx=mat[r][c-1];
-                i=r;
-                j=c-1;
-            }
-        }
-        return {i,j};
-    }
     vector<int> findPeakGrid(vector<vector<int>>& mat) {
         int m=mat.size();
         int n=mat[0].size();
-        int r=0,c=n-1;
-        while(r<m && c>=0){
-            pair<int,int>p=peak(m,n,r,c,mat);
-            int i=p.first;
-            int j=p.second;
-            if(i==r && j==c)
-            return {r,c};
-            else if(i==r-1 && j==c){
-                r=r-1;
+        int low=0,high=n-1;
+        while(low<=high){
+            int mid=(low+high)/2;
+            int indx=0;
+            int mx=mat[0][mid];
+            for(int j=0;j<m;j++){
+                if(mat[j][mid]>mx){
+                    mx=mat[j][mid];
+                    indx=j;
+                }
             }
-            else if(i==r+1 && j==c)
-            r=r+1;
-            else if(i==r && j==c-1)
-            c=c-1;
-            else if(i==r && j==c+1)
-            c=c+1;
+            if(mid>0 && mat[indx][mid-1]>mat[indx][mid])
+            high=mid-1;
+            else if(mid<n-1 && mat[indx][mid+1]>mat[indx][mid])
+            low=mid+1;
+            else
+            return {indx,mid};
         }
         return {-1,-1};
     }

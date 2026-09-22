0712c4d77d71c5ad009ae971d37f618c8f33347d
Harsh Sanjay Yeole <149146779+Harsh-Yeole@users.noreply.github.com>
2026-09-22 23:40:37 +0530

    Time: 0 ms (100%), Space: 20.5 MB (96%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.cpp
+++ 0045-jump-game-ii/0045-jump-game-ii.cpp
@@ -1,22 +1,17 @@
 class Solution {
 public:
-    int f(int n,int i,vector<int>&nums,vector<int>&dp){
-        if(i==nums.size()-1)
-        return 0;
-        if(nums[i]==0)
-        return 1e5;
-        if(dp[i]!=-1)
-        return dp[i];
-        int mn=1e5;
-        for(int j=i+1;j<=min(n-1,i+nums[i]);j++){
-            int cal=1+f(n,j,nums,dp);
-            mn=min(mn,cal);
-        }
-        return dp[i]=mn;
-    }
     int jump(vector<int>& nums) {
-        int n=nums.size();
-        vector<int>dp(n,-1);
-        return f(n,0,nums,dp);
+        int ans=0,n=nums.size();
+        int l=0,r=0;
+        while(r<n-1){
+            int j=0;
+            for(int k=l;k<=r;k++){
+                j=max(j,k+nums[k]);
+            }
+            r=j;
+            l=l+1;
+            ans++;
+        }
+        return ans;
     }
 };
